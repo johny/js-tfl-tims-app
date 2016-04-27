dfbce1c018c31d656208be4a716036dcefe414f6
Jan Kraus <kraus.jan@gmail.com>
2016-04-27 21:47:36 +0200

    Adds fetching XML via AJAX call

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,8 +1,18 @@
+class Disruption {
+  constructor(xmlNode) {
+    this.id = xmlNode.id;
+  }
+}
+
 class TflApp {
+
   constructor() {
-    console.log('App Initialized!');
+    this.apiUrl = 'https://data.tfl.gov.uk/tfl/syndication/feeds/tims_feed.xml?app_id=&app_key=';
+    this.disruptions = [];
+    this.map = null;
   }
 
+  // Runs when Google maps script is loaded, initialize empty map
   onMapInit() {
     this.map = new google.maps.Map(document.getElementById('tfl-map'), {
       center: {
@@ -12,6 +22,42 @@ class TflApp {
       scrollwheel: false,
       zoom: 13
     });
+
+    this.fetchData();
+  }
+
+  // fetches data from API via fetch api
+  // https://developers.google.com/web/updates/2015/03/introduction-to-fetch
+  fetchData() {
+    let request = new XMLHttpRequest();
+    request.addEventListener('load', this.onResponse.bind(this));
+    request.open("GET", this.apiUrl);
+    request.send()
+  }
+
+  // handle API response
+  onResponse(event) {
+    this.extractResults(event.target.responseXML);
+    this.renderToMap();
+  }
+
+  // handle API error
+  onError(err) {
+    console.log('Fetch error:', error);
+  }
+
+  // grab the results XML and store it in array
+  extractResults(xml) {
+    let disruptions = xml.getElementsByTagName('Disruption');
+
+    for (let i = 0; i < disruptions.length; i++) {
+      let d = disruptions[i];
+    }
+
+  }
+
+  renderToMap() {
+    console.log('Should render to map...')
   }
 
 }
@@ -21,10 +67,10 @@ class TflApp {
   'use strict';
 
   // initialize app
-  var app = new TflApp();
+  const app = new TflApp();
 
   // export map initialization function
-  window.initMap = app.onMapInit;
+  window.initMap = app.onMapInit.bind(app);
 
   // Your custom JavaScript goes here
 })();
