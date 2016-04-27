9e5416fdcd896fc00cedcd023ccdafb98eb9156a
Jan Kraus <kraus.jan@gmail.com>
2016-04-27 22:52:15 +0200

    Adds pin rendering to google map

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,9 +1,58 @@
+// Disruptionc lass for storing parsed data
 class Disruption {
-  constructor(xmlNode) {
-    this.id = xmlNode.id;
+  constructor(params) {
+    this.id = params.id;
+    this.status = params.status;
+    this.category = params.category;
+    this.comments = params.comments;
+
+    let coords = params.coordinates.split(',');
+
+    this.coordinates = {
+      lng: parseFloat(coords[0], 10),
+      lat: parseFloat(coords[1], 10)
+    };
   }
+
+  isActive() {
+    return this.status === 'Active';
+  }
+
 }
 
+// create Disruptions from XML nodes
+class DisruptionFactory {
+
+  createDisruptionFromXMLNode(node) {
+    let status = this.getNodeValue(node, 'status');
+    let category = this.getNodeValue(node, 'category');
+    let comments = this.getNodeValue(node, 'comments');
+
+    let coordinates = this.getCoordinates(node);
+
+    return new Disruption({
+      id: node.id,
+      status: status,
+      category: category,
+      comments: comments,
+      coordinates: coordinates
+    });
+  }
+
+  // simple search in node to find text content of desired child
+  getNodeValue(node, name) {
+    return node.getElementsByTagName(name)[0].textContent;
+  }
+
+  // find coordinates of disruption
+  getCoordinates(node) {
+    let displayPoint = node.getElementsByTagName('DisplayPoint')[0];
+    return displayPoint.getElementsByTagName('coordinatesLL')[0].textContent;
+  }
+
+}
+
+// Main App class
 class TflApp {
 
   constructor() {
@@ -14,11 +63,9 @@ class TflApp {
 
   // Runs when Google maps script is loaded, initialize empty map
   onMapInit() {
+    /* global google */
     this.map = new google.maps.Map(document.getElementById('tfl-map'), {
-      center: {
-        lat: 51.51,
-        lng: -0.1
-      },
+      center: {lat: 51.51, lng: -0.1},
       scrollwheel: false,
       zoom: 13
     });
@@ -31,8 +78,8 @@ class TflApp {
   fetchData() {
     let request = new XMLHttpRequest();
     request.addEventListener('load', this.onResponse.bind(this));
-    request.open("GET", this.apiUrl);
-    request.send()
+    request.open('GET', this.apiUrl);
+    request.send();
   }
 
   // handle API response
@@ -42,7 +89,7 @@ class TflApp {
   }
 
   // handle API error
-  onError(err) {
+  onError(error) {
     console.log('Fetch error:', error);
   }
 
@@ -50,14 +97,31 @@ class TflApp {
   extractResults(xml) {
     let disruptions = xml.getElementsByTagName('Disruption');
 
+    const disruptionFactory = new DisruptionFactory();
+
     for (let i = 0; i < disruptions.length; i++) {
       let d = disruptions[i];
+      this.disruptions.push(disruptionFactory.createDisruptionFromXMLNode(d));
     }
-
   }
 
+  // renders disruptions to map
   renderToMap() {
-    console.log('Should render to map...')
+    this.disruptions.forEach(function(d) {
+      // creating only markers for active disruptions
+      if (d.isActive()) {
+        this.addMapMarker(d);
+      }
+    }, this);
+  }
+
+  // creates single marker
+  addMapMarker(disruption) {
+    let marker = new google.maps.Marker({
+      position: disruption.coordinates,
+      map: this.map,
+      title: this.status
+    });
   }
 
 }
